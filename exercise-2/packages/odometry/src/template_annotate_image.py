0768526af50569d87e66bfb5a89f8eaceba201e7
TruongGiangPham-1 <truonggi@ualberta.ca>
2025-02-04 15:09:05 -0700

    fix: annotate camera fixeD

--- exercise-2/packages/odometry/src/template_annotate_image.py
+++ exercise-2/packages/odometry/src/template_annotate_image.py
@@ -6,7 +6,7 @@ import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
 from sensor_msgs.msg import CompressedImage
-
+import numpy as np
 import cv2
 from cv_bridge import CvBridge
 
@@ -28,7 +28,8 @@ class CameraReaderNode(DTROS):
         # construct subscriber
         self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
         # publish to the annotated image topic
-        self._publisher = rospy.Publisher(f"/{self._vehicle_name}/camera_node/image/gray_scale", CompressedImage, queue_size=10)
+        #self._publisher = rospy.Publisher(f"/{self._vehicle_name}/camera_node/image/gray_scale", CompressedImage, queue_size=10)
+        self._publisher = rospy.Publisher(self._camera_topic, CompressedImage, queue_size=10)
         pass
 
     def callback(self, msg):
@@ -36,12 +37,18 @@ class CameraReaderNode(DTROS):
 
         # convert JPEG bytes to CV image
         image = self._bridge.compressed_imgmsg_to_cv2(msg)
+        print(f'normal_img {image.shape} ')
         # convert to grayscale
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_image = np.stack((gray_image,)*3, axis=-1)
 
+        size = image.shape[0:2]
+        text = f"Duck {self._vehicle_name} says, ‘Cheese! Capturing {size} – quack-tastic!’"
 
-        compressed_image_msg = self._bridge.cv2_to_compressed_imgmsg(gray_image, dst_format="png")
-    
+        cv2.putText(gray_image, text, (0, size[1] // 2), fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 0.5, color=(250, 225, 100))
+
+        compressed_image_msg = self._bridge.cv2_to_compressed_imgmsg(gray_image, dst_format="jpg")
+        print(f'gray_img {gray_image.shape} {type(gray_image)}')
         # annotate the image
         # publish annotated grayscale image
         if not rospy.is_shutdown():
